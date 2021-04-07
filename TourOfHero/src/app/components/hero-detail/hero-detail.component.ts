c9939f0c074251548fb77e33cc5e8d4c55e32948
Phimbun Issawilanon <s6201012620198@email.kmutnb.ac.th>
2021-04-08 02:54:41 +0700

    detail page can display name and id hero

--- TourOfHero/src/app/components/hero-detail/hero-detail.component.ts
+++ TourOfHero/src/app/components/hero-detail/hero-detail.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit, Input } from "@angular/core";
-
+import { ActivatedRoute } from '@angular/router';
+import { Hero } from "~/app/hero";
+import { HeroService } from "../../hero.service";
 @Component ({
     selector: "HeroDetail",
     templateUrl: "./hero-detail.component.html",
 })
 
 export class HeroDetailComponent {
-    id: number;
-   name: string;
+    hero;
+   constructor(private route: ActivatedRoute,
+               private heroService: HeroService) {}
+   ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const id = Number(routeParams.get('id'));
+    this.hero = this.heroService.getHero(id);
+    console.log(this.hero);
+  }
+
 }
